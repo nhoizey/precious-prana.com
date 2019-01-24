f96562b5108398955b024439f36b18b3f59e037d
Nicolas Hoizey <nicolas@hoizey.com>
2019-01-24 23:55:40 +0100

    Prettierize the config

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,17 @@
 module.exports = function(config) {
-
   // Add a date formatter filter to Nunjucks
-  config.addFilter("dateDisplay", require("./filters/dates.js") );
-  config.addFilter("timestamp", require("./filters/timestamp.js") );
-  config.addFilter("squash", require("./filters/squash.js") );
+  config.addFilter('dateDisplay', require('./filters/dates.js'));
+  config.addFilter('timestamp', require('./filters/timestamp.js'));
+  config.addFilter('squash', require('./filters/squash.js'));
 
   return {
     dir: {
-      input: "src/site",
-      output: "dist",
-      includes: "_includes"
+      input: 'src/site',
+      output: 'dist',
+      includes: '_includes',
     },
-    templateFormats : ["njk", "md"],
-    htmlTemplateEngine : "njk",
-    markdownTemplateEngine : "njk"
+    templateFormats: ['njk', 'md'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
   };
-
 };
