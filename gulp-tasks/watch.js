4a3d81423b5df398c01c890e43e7676f255ffb9f
Nicolas Hoizey <nicolas@hoizey.com>
2019-06-15 00:52:10 +0200

    Prettier

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -1,11 +1,11 @@
-var project = require('./_project.js');
-var gulp = require('gulp');
+var project = require("./_project.js");
+var gulp = require("gulp");
 
 /*
   Watch folders for changess
 */
-gulp.task('watch', function() {
-  gulp.watch(project.buildSrc + '/js/**/*', gulp.parallel('scripts'));
-  gulp.watch(project.buildSrc + '/scss/**/*', gulp.parallel('styles'));
-  gulp.watch(project.buildSrc + '/site/**/*', gulp.parallel('generate'));
+gulp.task("watch", function() {
+  gulp.watch(project.buildSrc + "/js/**/*", gulp.parallel("scripts"));
+  gulp.watch(project.buildSrc + "/scss/**/*", gulp.parallel("styles"));
+  gulp.watch(project.buildSrc + "/**/*", gulp.parallel("generate"));
 });
