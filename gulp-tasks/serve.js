42c01594ef33b86beec8f6e6e2215c927fce1753
Nicolas Hoizey <nicolas@hoizey.com>
2019-06-24 19:45:28 +0200

    Use port 8080 to be compatible with Forestry previews

--- gulp-tasks/serve.js
+++ gulp-tasks/serve.js
@@ -1,11 +1,14 @@
-var project = require('./_project.js');
-var gulp    = require('gulp');
-var serve   = require('gulp-serve');
+var project = require("./_project.js");
+var gulp = require("gulp");
+var serve = require("gulp-serve");
 
 /*
  local webserver for development
 */
-gulp.task('serve', serve({
-  root: [project.buildDest],
-  port: 8008,
-}));
+gulp.task(
+  "serve",
+  serve({
+    root: [project.buildDest],
+    port: 8080
+  })
+);
