f7aa0115540ab0d4b2148337ff43e1243fceff6f
Nicolas Hoizey <nicolas@hoizey.com>
2019-10-14 16:03:17 +0200

    Fix issue with figures

--- .eleventy.js
+++ .eleventy.js
@@ -28,48 +28,49 @@ module.exports = function(eleventyConfig) {
       "https://res.cloudinary.com/nho/image/fetch/c_fill,f_auto,q_auto,";
     let imageUrl = "https://precious-prana.com" + image.src;
 
-    return `<figure class="${cssClass}" ${
-      image.width && image.height
-        ? `style="max-width: ${image.width}px; --aspect-ratio: ${image.width /
-            image.height}"`
-        : ""
-    }>
-  <img
-    src="${cloudinaryPrefix}w_360/${imageUrl}"
-    srcset="
-  ${cloudinaryPrefix}w_360/${imageUrl} 360w
-  ${
-    !image.width || image.width >= 480
-      ? `, ${cloudinaryPrefix}w_480/${imageUrl} 480w`
-      : ""
-  }
-  ${
-    !image.width || image.width >= 640
-      ? `, ${cloudinaryPrefix}w_640/${imageUrl} 640w`
-      : ""
-  }
-  ${
-    !image.width || image.width >= 800
-      ? `, ${cloudinaryPrefix}w_800/${imageUrl} 800w`
-      : ""
-  }
-  ${
-    !image.width || image.width >= 1024
-      ? `, ${cloudinaryPrefix}w_1024/${imageUrl} 1024w`
-      : ""
-  }"
-    sizes="${sizes}"
-    ${image.alt ? `alt="${image.alt}"` : ""}
-    ${image.width ? `width="${image.width}"` : ""} />
-  <figcaption>
-  ${image.caption ? `<p>${image.caption}</p>` : ""}
-  ${
-    image.zoom
-      ? `<p class="zoom">&#128269;&nbsp;<a href="${image.src}" target="_blank">zoomer</a></p>`
-      : ""
-  }
-  </figcaption>
-</figure>`;
+    let html = `<figure class="${cssClass}" `;
+    if (image.width && image.height) {
+      html += `style="max-width: ${
+        image.width
+      }px; --aspect-ratio: ${image.width / image.height}"`;
+    }
+    html += `>
+<img
+  src="${cloudinaryPrefix}w_360/${imageUrl}"
+  srcset="${cloudinaryPrefix}w_360/${imageUrl} 360w`;
+    if (!image.width || image.width >= 480) {
+      html += `, ${cloudinaryPrefix}w_480/${imageUrl} 480w`;
+    }
+    if (!image.width || image.width >= 640) {
+      html += `, ${cloudinaryPrefix}w_640/${imageUrl} 640w`;
+    }
+    if (!image.width || image.width >= 800) {
+      html += `, ${cloudinaryPrefix}w_800/${imageUrl} 800w`;
+    }
+    if (!image.width || image.width >= 1024) {
+      html += `, ${cloudinaryPrefix}w_1024/${imageUrl} 1024w`;
+    }
+    html += `" sizes="${sizes}"`;
+    if (image.alt) {
+      html += ` alt="${image.alt}"`;
+    }
+    if (image.width) {
+      html += ` width="${image.width}"`;
+    }
+    html += ` />`;
+    if (image.caption || image.zoom) {
+      html += `<figcaption>`;
+      if (image.caption) {
+        html += `<p>${image.caption}</p>`;
+      }
+      if (image.zoom) {
+        html += `<p class="zoom">&#128269;&nbsp;<a href="${image.src}" target="_blank">zoomer</a></p>`;
+      }
+      html += `</figcaption>`;
+    }
+    html += `</figure>`;
+
+    return html;
   }
 
   eleventyConfig.addNunjucksShortcode("image", function(image) {
