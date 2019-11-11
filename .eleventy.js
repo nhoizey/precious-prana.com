9fc485ef1c9ca619912ec29a68a693e90d6477ff
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-11 22:44:13 +0100

    Use local URL in development mode

--- .eleventy.js
+++ .eleventy.js
@@ -29,53 +29,57 @@ module.exports = function (eleventyConfig) {
   // ------------------------------------------------------------------------
 
   function genericImage(image, cssClass, sizes) {
-    const cloudinaryPrefix =
-      "https://res.cloudinary.com/nho/image/fetch/c_fill,f_auto,q_auto,";
-    let imageUrl = "https://precious-prana.com" + image.src;
+    if (process.env.ELEVENTY_ENV === 'development') {
+      return `<img src="${image.src}" class="${cssClass}" />`;
+    } else {
+      const cloudinaryPrefix =
+        "https://res.cloudinary.com/nho/image/fetch/c_fill,f_auto,q_auto,";
+      let imageUrl = "https://precious-prana.com" + image.src;
 
-    let html = `<figure class="${cssClass}" `;
-    if (image.width && image.height) {
-      html += `style="max-width: ${
-        image.width
-        }px; --aspect-ratio: ${image.width / image.height}"`;
-    }
-    html += `>
+      let html = `<figure class="${cssClass}" `;
+      if (image.width && image.height) {
+        html += `style="max-width: ${
+          image.width
+          }px; --aspect-ratio: ${image.width / image.height}"`;
+      }
+      html += `>
 <img
   src="${cloudinaryPrefix}w_360/${imageUrl}"
   srcset="${cloudinaryPrefix}w_360/${imageUrl} 360w`;
-    if (!image.width || image.width >= 480) {
-      html += `, ${cloudinaryPrefix}w_480/${imageUrl} 480w`;
-    }
-    if (!image.width || image.width >= 640) {
-      html += `, ${cloudinaryPrefix}w_640/${imageUrl} 640w`;
-    }
-    if (!image.width || image.width >= 800) {
-      html += `, ${cloudinaryPrefix}w_800/${imageUrl} 800w`;
-    }
-    if (!image.width || image.width >= 1024) {
-      html += `, ${cloudinaryPrefix}w_1024/${imageUrl} 1024w`;
-    }
-    html += `" sizes="${sizes}"`;
-    if (image.alt) {
-      html += ` alt="${image.alt}"`;
-    }
-    if (image.width) {
-      html += ` width="${image.width}"`;
-    }
-    html += ` />`;
-    if (image.caption || image.zoom) {
-      html += `<figcaption>`;
-      if (image.caption) {
-        html += `<p>${image.caption}</p>`;
+      if (!image.width || image.width >= 480) {
+        html += `, ${cloudinaryPrefix}w_480/${imageUrl} 480w`;
       }
-      if (image.zoom) {
-        html += `<p class="zoom">&#128269;&nbsp;<a href="${image.src}" target="_blank">zoomer</a></p>`;
+      if (!image.width || image.width >= 640) {
+        html += `, ${cloudinaryPrefix}w_640/${imageUrl} 640w`;
       }
-      html += `</figcaption>`;
-    }
-    html += `</figure>`;
+      if (!image.width || image.width >= 800) {
+        html += `, ${cloudinaryPrefix}w_800/${imageUrl} 800w`;
+      }
+      if (!image.width || image.width >= 1024) {
+        html += `, ${cloudinaryPrefix}w_1024/${imageUrl} 1024w`;
+      }
+      html += `" sizes="${sizes}"`;
+      if (image.alt) {
+        html += ` alt="${image.alt}"`;
+      }
+      if (image.width) {
+        html += ` width="${image.width}"`;
+      }
+      html += ` />`;
+      if (image.caption || image.zoom) {
+        html += `<figcaption>`;
+        if (image.caption) {
+          html += `<p>${image.caption}</p>`;
+        }
+        if (image.zoom) {
+          html += `<p class="zoom">&#128269;&nbsp;<a href="${image.src}" target="_blank">zoomer</a></p>`;
+        }
+        html += `</figcaption>`;
+      }
+      html += `</figure>`;
 
-    return html;
+      return html;
+    }
   }
 
   eleventyConfig.addNunjucksShortcode("image", function (image) {
