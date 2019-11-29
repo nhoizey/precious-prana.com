008bde36fb39168ae9f810b12f8cc6ee9ec290eb
Nicolas Hoizey <nicolas@hoizey.com>
2019-11-29 14:08:42 +0100

    Clean useless code

--- src/_filters/plain-date.js
+++ src/_filters/plain-date.js
@@ -1,33 +1,7 @@
 const { DateTime } = require("luxon");
 
-const appendSuffix = n => {
-  return n + (n === 1 ? "er" : "");
-};
-
 module.exports = function plainDate(value) {
   return DateTime.fromJSDate(value, { zone: "Europe/Paris" })
     .setLocale("fr")
     .toLocaleString(DateTime.DATE_FULL);
-
-  // const dateObject = new Date(value);
-
-  // const months = [
-  //   "janvier",
-  //   "février",
-  //   "mars",
-  //   "avril",
-  //   "mai",
-  //   "juin",
-  //   "juillet",
-  //   "août",
-  //   "septembre",
-  //   "octobre",
-  //   "novembre",
-  //   "décembre"
-  // ];
-  // const dayWithSuffix = appendSuffix(dateObject.getDate());
-
-  // return `${dayWithSuffix} ${
-  //   months[dateObject.getMonth()]
-  // } ${dateObject.getFullYear()}`;
 };
