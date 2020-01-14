2f3214d876cb62591192f1ef796f006f2639f534
Nicolas Hoizey <nicolas@hoizey.com>
2020-01-14 23:53:11 +0100

    Add site meta data

---
+++ src/_data/site.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "Precious Prana",
+  url: "https://precious-prana.com",
+  title: "Faire pétiller",
+  description: "",
+  author: {
+    name: "Jacqueline Oud",
+    email: "jacqueline@precious-prana.com",
+  }
+};
